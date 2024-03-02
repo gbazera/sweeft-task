ba888ac5c4c3e20dde74de4dc97a64713a1f1860
Giorgi Bazerashvili <bazerashvili.gio27@gmail.com>
2024-03-02 20:46:15 +0400

    Fixed dependencies

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -77,7 +77,7 @@ const Home: React.FC = () => {
 
     window.addEventListener('scroll', handleScroll)
     return () => window.removeEventListener('scroll', handleScroll)
-  }, [isSearching])
+  }, [isSearching, refetchSearched])
 
   return (
     <main>
