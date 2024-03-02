fb52887bb6fc4540074e2707bd865debf2981778
Giorgi Bazerashvili <bazerashvili.gio27@gmail.com>
2024-03-02 21:06:30 +0400

    Changed BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
@@ -11,13 +11,13 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <Router>
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/history' element={<History />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </QueryClientProvider>
   );
 }
